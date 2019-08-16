723be1e8017a0a33ec373b1059a1d11464909be3
Kevin Liu <yq.kevin.liu@gmail.com>
2019-08-16 19:21:45 -0400

    navbar hide on mobile after click (JS), increased size

--- javascript.js
+++ javascript.js
@@ -1,18 +1,9 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(function() {
+  $('.nav-disappear').on('click', function(){ 
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active1");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+      
+      if($('.navbar-toggler').css('display') !='none'){
+          $('.navbar-toggler').trigger( "click" );
+      }
   });
-}
+});
